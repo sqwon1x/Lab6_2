924bcf6ee733b5d50c64782a443bf7b31a2858eb
Mykhailo Hutych <miichail.gutych@gmail.com>
2026-02-19 23:47:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Security.Claims;
+
+// Головна програма
+class Program
+{
+    static void Main()
+    {
+        List<Furniture> furnitureList = new List<Furniture>
+        {
+            new Chair("Wood"),
+            new Table("Metal"),
+            new Wardrobe("Oak")
+        };
+
+        foreach (Furniture item in furnitureList)
+        {
+            item.Assemble();
+            Console.WriteLine();
+        }
+    }
+}
